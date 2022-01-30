e0b065229dd2a3f834c811bbad75faf9b9a73f9e
kyriakatos1971 <kyriakos.kyriakatos@outlook.com>
2022-01-30 23:12:01 +0100

    renamed basic folder

---
+++ Rust_Examples/calculator/target/rls/debug/deps/calculator-29398c60638bc86f.d
@@ -0,0 +1,5 @@
+/Users/kyriakoskyriakatos/coding_projects/Rust_Examples/calculator/target/rls/debug/deps/calculator-29398c60638bc86f.rmeta: src/main.rs
+
+/Users/kyriakoskyriakatos/coding_projects/Rust_Examples/calculator/target/rls/debug/deps/calculator-29398c60638bc86f.d: src/main.rs
+
+src/main.rs:
---
+++ Rust_Examples/calculator/target/rls/debug/deps/calculator-3fad0869ff0100ef.d
@@ -0,0 +1,5 @@
+/Users/kyriakoskyriakatos/coding_projects/Rust_Examples/calculator/target/rls/debug/deps/calculator-3fad0869ff0100ef.rmeta: src/main.rs
+
+/Users/kyriakoskyriakatos/coding_projects/Rust_Examples/calculator/target/rls/debug/deps/calculator-3fad0869ff0100ef.d: src/main.rs
+
+src/main.rs:
